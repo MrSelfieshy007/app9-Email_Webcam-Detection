5fd14a4f0fe2bb2f707de588b119b87f367a058b
hnadig <harshithnadig1@gmail.com>
2023-07-01 20:14:09 +0530

    added gaussian blur and grayscale

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
-    cv2.imshow("My Video",frame)
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame,(21,21),0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame,gray_frame_gau)
+    cv2.imshow("My Video", delta_frame)
 
     key = cv2.waitKey(1)
 
@@ -15,5 +24,4 @@ while True:
 
 video.release()
 
-print(check)
-print(frame)
+
